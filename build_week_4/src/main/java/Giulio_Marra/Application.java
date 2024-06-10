ce670322658ffca8ecc91e369a08e3abb64de18a
Giulio-Marra <giuliomarramail@gmail.com>
2024-06-10 11:48:42 +0200

    test

--- build_week_4/src/main/java/Giulio_Marra/Application.java
+++ build_week_4/src/main/java/Giulio_Marra/Application.java
@@ -3,6 +3,6 @@ package Giulio_Marra;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello ciao buongioro0sdfsdfsdf!");
     }
 }
