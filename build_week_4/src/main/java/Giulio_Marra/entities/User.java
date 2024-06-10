d4ed71a8231c64de3f0dd58d5143c0b520faa173
Giulio-Marra <giuliomarramail@gmail.com>
2024-06-10 11:54:03 +0200

    test3

---
+++ build_week_4/src/main/java/Giulio_Marra/entities/User.java
@@ -0,0 +1,13 @@
+package Giulio_Marra.entities;
+
+public class User {
+    private long id;
+    private String name;
+
+    public User(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    
+
+}
